a1c29ebab664b355547acc6286237d9575d2f812
aditya-builds07 <aditya.builds07@gmail.com>
2026-09-20 16:53:59 +0530

    debugging Directories

--- backend/src/routes/crop.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getCrops } from "../controllers/crop.controller.js";
-
-export const cropRouter = Router();
-
-cropRouter.get("/crops", getCrops);
--- backend/tests/simulation/simulation.test.js
+++
@@ -1,9 +0,0 @@
-// KrishiMitra - Initial Simulation Smoke Test
-describe('KrishiMitra Architecture Setup', () => {
-  it('should verify test runner environment is functional', () => {
-    const isReady = true;
-    if (!isReady) {
-      throw new Error('Environment not ready');
-    }
-  });
-});
